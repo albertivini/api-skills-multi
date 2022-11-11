0ef08bb6f734d8c9b598814a063bacd08326b932
Vinicius Alberti <paralvlup.cal@gmail.com>
2022-11-10 21:52:18 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
-import express, { json } from "express"
+import express, { json, NextFunction, Request, Response } from "express"
+import cors from "cors"
 import { routes } from "./routes/routes"
 
 const app = express()
 
 app.use(json())
 app.use(routes)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+    app.use(cors())
+    next()
+})
 
 app.listen(process.env.PORT || 3000)
 
